769c4ff8b3212e1e739d82de18c70c86ad2765d2
natob <natob@REBARROS>
2023-11-03 21:57:51 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Jogo_memoria_com_Formas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTempoRestante = new System.Windows.Forms.Label();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.TempoJogo = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(534, 183);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(127, 16);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Tentativas Erradas: ";
+            // 
+            // lblTempoRestante
+            // 
+            this.lblTempoRestante.AutoSize = true;
+            this.lblTempoRestante.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTempoRestante.Location = new System.Drawing.Point(523, 248);
+            this.lblTempoRestante.Name = "lblTempoRestante";
+            this.lblTempoRestante.Size = new System.Drawing.Size(138, 29);
+            this.lblTempoRestante.TabIndex = 0;
+            this.lblTempoRestante.Text = "Tempo: 30";
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.BackColor = System.Drawing.Color.Silver;
+            this.btnRestar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestar.ForeColor = System.Drawing.Color.Black;
+            this.btnRestar.Location = new System.Drawing.Point(528, 62);
+            this.btnRestar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(127, 37);
+            this.btnRestar.TabIndex = 1;
+            this.btnRestar.Text = "Restart";
+            this.btnRestar.UseVisualStyleBackColor = false;
+            this.btnRestar.Click += new System.EventHandler(this.RestartGameEvent);
+            // 
+            // TempoJogo
+            // 
+            this.TempoJogo.Interval = 1000;
+            this.TempoJogo.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.lblTempoRestante);
+            this.Controls.Add(this.lblStatus);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Jogo da Memoria PLUS 5000";
+ 
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblTempoRestante;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Timer TempoJogo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_memoria_com_Formas
+{
+    public partial class Form1 : Form
+    {
+
+
+        List<int> numbers = new List<int> { 1,1,2,2,3,3,4,4,5,5,6,6};
+        string firstChoice;
+        string secondChoice;
+        int tries;
+        List<PictureBox> pictures = new List<PictureBox>();
+        PictureBox picA;
+        PictureBox picB;
+        int totaTime = 30;
+        int countDownTime;
+        bool fimJogo = false;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPictures();
+
+        }
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            countDownTime--;
+
+            lblTempoRestante.Text = "Tempo Restante: " + countDownTime;
+
+            if (countDownTime < 1)
+            {
+                FimJogo("Tempo acabou, Você perdeu");
+
+                foreach (PictureBox x in pictures)
+                {
+                    if (x.Tag != null)
+                    {
+                        x.Image = Image.FromFile("pics/" + (string)x.Tag + ".png");
+                    }
+                }
+            }
+        }
+
+        private void LoadPictures()
+        {
+            int leftPos = 20;
+            int topPos = 20;
+            int rows = 0;
+
+            for (int i = 0; i < 12; i++)
+            {
+                PictureBox newPic = new PictureBox();
+                newPic.Height = 50;
+                newPic.Width = 50;
+                newPic.BackColor = Color.LightGray;
+                newPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                newPic.Click += NewPic_click;
+                pictures.Add(newPic);
+
+                if (rows < 3)
+                {
+                    rows++;
+                    newPic.Left = leftPos;
+                    newPic.Top = topPos;
+                    this.Controls.Add(newPic);
+                    leftPos = leftPos + 60;
+                }
+                if (rows == 3)
+                {
+                    leftPos = 20;
+                    topPos += 60;
+                    rows = 0;
+                }
+
+
+            }
+            RestartGame();
+        }
+
+        private void NewPic_click(object sender, EventArgs e)
+        {
+            if (fimJogo)
+            {
+                //não registar o click 
+                return;
+            }
+
+            if(firstChoice == null)
+            {
+                picA = sender as PictureBox;
+                if (picA.Tag != null && picA.Image == null)
+                {
+                    picA.Image = Image.FromFile("pics/" + (string)picA.Tag + ".png");
+                    firstChoice = (string)picA.Tag;
+
+                }
+            }
+            else if(secondChoice == null)
+            {
+                picB = sender as PictureBox;
+
+                if(picB.Tag != null && picB.Image == null)
+                {
+                    picB.Image = Image.FromFile("pics/" + (string)picB.Tag + ".png");
+                    secondChoice = (string)picB.Tag;
+                }
+            }
+            else
+            {
+                CheckPictures(picA, picB);
+            }
+        } 
+               
+
+        private void RestartGameEvent(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void RestartGame()
+        {   
+            //randon na lista
+            var randomList = numbers.OrderBy(x => Guid.NewGuid()).ToList();
+
+            //alterando a lista aletoria para original
+            numbers = randomList;
+
+            for (int i = 0; i < pictures.Count; i++)
+            {
+                pictures[i].Image = null;
+                pictures[i].Tag = numbers[i].ToString();
+            }
+
+            tries = 0;
+            lblStatus.Text = "Tentativas: " + tries;
+            lblTempoRestante.Text = "Tempo Restate: " + totaTime;
+            fimJogo = false;
+            TempoJogo.Start();
+            countDownTime = totaTime;                    
+
+        }
+
+        private void CheckPictures(PictureBox A, PictureBox B)
+        {
+            if(firstChoice == secondChoice)
+            {
+                A.Tag = null;
+                B.Tag = null;
+            }
+            else
+            {
+                tries++;
+                lblStatus.Text = "Tentativas erradas " + tries + " vezes.";
+            }
+
+            firstChoice = null;
+            secondChoice = null;
+
+            foreach (PictureBox pics in pictures.ToList())
+            {
+                if (pics.Tag != null)
+                {
+                    pics.Image = null;
+                }
+            }
+
+            // verificar se todos os itens foram resolvidos
+
+            if (pictures.All(o => o.Tag == pictures[0].Tag))
+            {
+                FimJogo("Bom trabalho, Você ganhou!!");
+            }
+
+        }
+
+        private void FimJogo(string msg)
+        {
+            TempoJogo.Stop();
+            fimJogo = true;
+            MessageBox.Show(msg + "Click em Restar para jogar novamente");
+        }
+
+      
+
+       
+    }
+}
